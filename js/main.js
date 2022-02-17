dd4b19f377179ba4ee533914373e2d558542d9d5
muzahid604 <604muzahid@gmail.com>
2022-02-17 21:45:14 +0600

    error massager added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
-
-function balance() {
+// income function
+function income() {
     const incomeInput = document.getElementById('income-input');
     const incomeMoney = parseFloat(incomeInput.value);
-    const balance = incomeMoney - totalExpenses();
-    return balance;
+    if (incomeMoney > 0) {
+        const balance = incomeMoney - totalExpenses();
+        return balance;
+    }
+    else {
+        const balance = 'Income some money then you have some Balance'
+        return balance;
+    }
 }
+// totalExpenses function
 function totalExpenses() {
     const foodInput = document.getElementById('food-input');
     const foodCost = parseFloat(foodInput.value);
@@ -12,30 +19,44 @@ function totalExpenses() {
     const rentCost = parseFloat(rentInput.value);
     const clothesInput = document.getElementById('clothes-input');
     const clothesCost = parseFloat(clothesInput.value);
-    const totalExpenses = foodCost + rentCost + clothesCost;
-    return totalExpenses;
+    if (foodCost > 0 && rentCost > 0 && clothesCost > 0) {
+        const totalExpenses = foodCost + rentCost + clothesCost;
+        return totalExpenses;
+    }
+    else {
+        const totalExpenses = 'Please Enter Valid Input'
+        return totalExpenses
+    }
 }
-
+// calculation function
 function calculation() {
     document.getElementById('total-expenses-span').innerText = totalExpenses();
-    document.getElementById('balance').innerText = balance();
+    document.getElementById('balance').innerText = income();
     const balanceForSave = balance;
     return balanceForSave;
 }
+// savings function
 function savings() {
     const saveInput = document.getElementById('save-input');
     const savePercent = saveInput.value;
-    const save = (balance() * savePercent) / 100;
-    document.getElementById('save-span').innerText = save;
-    const remainingBalance = balance() - save;
-    document.getElementById('remainig-span').innerText = remainingBalance;
+    if (savePercent > 0) {
+        const save = (income() * savePercent) / 100;
+        document.getElementById('save-span').innerText = save;
+        const remainingBalance = income() - save;
+        document.getElementById('remainig-span').innerText = remainingBalance;
+    }
+    else {
+        document.getElementById('remainig-span').innerText = 'please enter a valid percent';
+        document.getElementById('save-span').innerText = 'You have no savings';
+    }
 }
+// calculate button handeler
 const calculateButton = document.getElementById('calculate-btn')
 calculateButton.addEventListener('click', function () {
     calculation();
 })
+// save button handle 
 const savingButton = document.getElementById('save-button');
 savingButton.addEventListener('click', function () {
-    calculation();
     savings();
 })
