33811ac76c300bb11f6b364b675d81568f09a292
muzahid604 <604muzahid@gmail.com>
2022-02-17 13:30:17 +0600

    calculation inputs value added

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const calculateButton = document.getElementById('calculate-btn')
+calculateButton.addEventListener('click', function () {
+    const incomeInput = document.getElementById('income-input');
+    const incomeMoney = incomeInput.value;
+    const foodInput = document.getElementById('food-input');
+    const foodCost = foodInput.value;
+    const rentInput = document.getElementById('rent-input');
+    const rentCost = rentInput.value;
+    const clothesInput = document.getElementById('clothes-input');
+    const clothesCost = clothesInput.value;
+    const foodInput = document.getElementById('food-input');
+    const foodCost = foodInput.value;
+})
