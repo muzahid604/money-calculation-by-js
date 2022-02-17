f0f644f28a084e0f7d169848adeb4c63805678a7
muzahid604 <604muzahid@gmail.com>
2022-02-17 14:51:14 +0600

    saving percent is worked

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-const calculateButton = document.getElementById('calculate-btn')
-calculateButton.addEventListener('click', function () {
+function calculation(num) {
     const incomeInput = document.getElementById('income-input');
     const incomeMoney = parseFloat(incomeInput.value);
     const foodInput = document.getElementById('food-input');
@@ -12,8 +11,20 @@ calculateButton.addEventListener('click', function () {
     const remainingBalance = incomeMoney - totalExpenses;
     document.getElementById('total-expenses-span').innerText = totalExpenses;
     document.getElementById('balance').innerText = remainingBalance;
+    const remainingBalanceForSave = remainingBalance;
+    const save = (remainingBalanceForSave * num) / 100;
+    document.getElementById('save-span').innerText = save;
+
+}
+const calculateButton = document.getElementById('calculate-btn')
+calculateButton.addEventListener('click', function () {
+    calculation();
 })
 const savingButton = document.getElementById('save-button');
 savingButton.addEventListener('click', function () {
+    const saveInput = document.getElementById('save-input');
+    const savePercent = saveInput.value;
+    calculation(savePercent);
+    // console.log(save)
 
 })
